2478f2430cbc677eb340dab4f6c0cd36f912b16e
nedretcelik <nedretcelik97@gmail.com>
2024-05-04 21:37:26 -0400

    add calculation for number of days

--- scripts/book.js
+++ scripts/book.js
@@ -1,2 +1,40 @@
-"use strict":
+"use strict";
 
+window.onload = function () {
+    const bookSubmitBtn = document.getElementById("bookSubmitBtn");
+    const checkInDate = document.getElementById("checkInDate");
+    const checkOutDate = document.getElementById("checkOutDate");
+
+
+    checkInDate.onchange = numberOfNightsCalculation;
+    checkOutDate.onchange = numberOfNightsCalculation;
+
+
+    bookSubmitBtn.onclick = getRoomRate;
+
+}
+
+
+function numberOfNightsCalculation() {
+    const checkInDate = new Date(document.getElementById("checkInDate").value);
+    const checkOutDate = new Date(document.getElementById("checkOutDate").value);
+    const numberOfNights = document.getElementById("numberOfNights");
+
+
+    let msecPerDay = 1000 * 60 * 60 * 24;
+    let elapsedMilliseconds = checkOutDate.getTime() - checkInDate.getTime()
+
+    let dayDiff = elapsedMilliseconds / msecPerDay;
+
+    
+    numberOfNights.value = dayDiff;
+
+    return numberOfNights.value;
+
+}
+
+function getRoomRate() {
+    
+ 
+
+}
